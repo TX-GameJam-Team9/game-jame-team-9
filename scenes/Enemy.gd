3204415bf1b50ea5ee3cf670a6dc84ed7acfcc97
NameInWorkshop <kennyferg06@gmail.com>
2025-09-27 22:44:56 -0500

    Added enemy pathing

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,13 +1,23 @@
 extends Area2D
 
+@onready var player: Node2D = get_node("../../Player")
+@onready var nav : NavigationAgent2D = $NavigationAgent2D
+var finished = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
+	update_path()
 
+	# Set Destination
+func update_path():
+	nav.target_position = player.global_position
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if not finished:
+		update_path()
+		var direction = (nav.get_next_path_position() - global_position).normalized()
+		translate(direction * 200 * delta)
+	
